82d1e130b94de8646ac3de04894b63f4dbf8d1bd
tc-imba <liuyh615@sjtu.edu.cn>
2020-08-23 02:16:26 +0800

    add record and problem

---
+++ joj/horse/models/problem.py
@@ -0,0 +1,30 @@
+from typing import Optional, List
+
+from joj.horse.odm import Document, Reference
+from joj.horse.models.domain import DomainReference
+from joj.horse.models.user import UserReference
+
+
+class Problem(Document):
+    class Mongo:
+        collection = "problems"
+        indexes = [
+        ]
+
+    domain: DomainReference
+    owner: UserReference
+
+    title: str
+    content: str = ""
+    hidden: bool = False
+    num_submit: int = 0
+    num_accept: int = 0
+
+    data: Optional[int] = None  # modify later
+    data_version: int = 2
+    languages: List[str] = []
+
+
+class ProblemReference(Reference):
+    data: Optional[Problem] = None
+    reference = Problem
---
+++ joj/horse/models/record.py
@@ -0,0 +1,79 @@
+from enum import IntEnum
+from typing import List, Optional
+from datetime import datetime
+
+from pydantic import BaseModel
+from pymongo import IndexModel, ASCENDING, DESCENDING
+
+from joj.horse.odm import Document
+
+from joj.horse.models.user import UserReference
+from joj.horse.models.domain import DomainReference
+from joj.horse.models.problem import ProblemReference
+
+
+class RecordStatus(IntEnum):
+    waiting = 0
+    accepted = 1
+    wrong_answer = 2
+    time_limit_exceeded = 3
+    memory_limit_exceeded = 4
+    output_limit_exceeded = 5
+    runtime_error = 6
+    compile_error = 7
+    system_error = 8
+    canceled = 9
+    etc = 10
+    judging = 20
+    compiling = 21
+    fetched = 22
+    ignored = 30
+
+
+class RecordCodeType(IntEnum):
+    text = 0
+    tar = 1
+    zip = 2
+    rar = 3
+
+
+class RecordCase(BaseModel):
+    status: RecordStatus
+    score: int = 0
+    time_ms: int = 0
+    memory_kb: int = 0
+    execute_status: int = 0
+    stdout: str = ""
+    stderr: str = ""
+
+
+class Record(Document):
+    class Mongo:
+        collection = "records"
+        indexes = [
+            IndexModel([("domain", ASCENDING), ("problem", ASCENDING), ("user", ASCENDING), ("submit_at", DESCENDING)]),
+            IndexModel([("problem", ASCENDING), ("user", ASCENDING), ("submit_at", DESCENDING)]),
+            IndexModel([("domain", ASCENDING), ("user", ASCENDING), ("submit_at", DESCENDING)]),
+            IndexModel([("user", ASCENDING), ("submit_at", DESCENDING)]),
+        ]
+
+    status: RecordStatus
+    score: int = 0
+    time_ms: int = 0
+    memory_kb: int = 0
+    domain: DomainReference
+    problem: ProblemReference
+    problem_set: Optional[int] = None  # modify later
+    problem_data: int  # modify later
+    user: UserReference
+    code_type: RecordCodeType
+    code: str  # modify later
+    judge_category: List[str]
+
+    submit_at: datetime
+    judge_at: datetime
+
+    judge_user: UserReference
+
+    compiler_texts: str = ""
+    cases: List[RecordCase] = []
