e80cd45e56b272700c844e036e68066d22531552
tc-imba <liuyh615@sjtu.edu.cn>
2021-02-05 02:18:29 +0800

    add /domain/{domain}

--- joj/horse/models/domain.py
+++ joj/horse/models/domain.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from pymongo import IndexModel
 from umongo import fields
 from umongo.frameworks.motor_asyncio import MotorAsyncIODocument
@@ -6,8 +7,14 @@ from joj.horse.models.user import User
 from joj.horse.utils.db import instance
 
 
+class DocumentMixin:
+    @classmethod
+    async def find_by_id(cls, _id):
+        return await cls.find_one({'_id': ObjectId(_id)})
+
+
 @instance.register
-class Domain(MotorAsyncIODocument):
+class Domain(MotorAsyncIODocument, DocumentMixin):
     class Meta:
         collection_name = "domains"
         indexes = [
@@ -24,3 +31,11 @@ class Domain(MotorAsyncIODocument):
 
     gravatar = fields.StringField(default="")
     bulletin = fields.StringField(default="")
+
+    @classmethod
+    async def find_by_url_or_id(cls, url_or_id: str) -> 'Domain':
+        try:
+            _id = ObjectId(url_or_id)
+            return await cls.find_one({'_id': ObjectId(_id)})
+        except:
+            return await cls.find_one({'url': url_or_id})
