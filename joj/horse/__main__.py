fac6ee0a1eced7ec1d2aa66b23b1f9823b336971
tc-imba <liuyh615@sjtu.edu.cn>
2021-02-10 02:50:54 +0800

    add logging config

--- joj/horse/__main__.py
+++ joj/horse/__main__.py
@@ -6,10 +6,13 @@ from joj.horse.utils.cli import cli_command
 
 @cli_command()
 def main():
+    log_config = uvicorn.config.LOGGING_CONFIG
+    log_config["formatters"]["default"]["fmt"] = "[%(levelname)1.1s %(asctime)s %(filename)s:%(lineno)d] %(message)s"
+    log_config["formatters"]["default"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
     uvicorn.run(
         "joj.horse:app",
         host=settings.host, port=settings.port,
-        reload=settings.debug
+        reload=settings.debug, log_config=log_config,
     )
 
 
