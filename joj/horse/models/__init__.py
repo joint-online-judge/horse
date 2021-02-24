1ac0245ff61ab7bc1cf26fd7fca0208c5ca58f9c
BoYanZh <bomingzh@sjtu.edu.cn>
2021-02-25 02:42:44 +0800

    more document in collections

--- joj/horse/models/__init__.py
+++ joj/horse/models/__init__.py
@@ -2,3 +2,6 @@ from joj.horse.models.domain import Domain
 from joj.horse.models.domain_role import DomainRole
 from joj.horse.models.domain_user import DomainUser
 from joj.horse.models.user import User
+from joj.horse.models.record import Record
+from joj.horse.models.problem import Problem
+from joj.horse.models.problem_set import ProblemSet
