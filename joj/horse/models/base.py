2b6df8142e9a43bba0d0c64ab25e0b3bc2fba356
BoYanZh <bomingzh@sjtu.edu.cn>
2021-02-25 18:26:45 +0800

    add validator to find_by_id

--- joj/horse/models/base.py
+++ joj/horse/models/base.py
@@ -1,7 +1,11 @@
 from bson import ObjectId
 
+from joj.horse.utils.errors import UnprocessableEntityError
+
 
 class DocumentMixin:
     @classmethod
     async def find_by_id(cls, _id):
+        if not ObjectId.is_valid(_id):
+            raise UnprocessableEntityError("Invalid ObjectId")
         return await cls.find_one({"_id": ObjectId(_id)})
