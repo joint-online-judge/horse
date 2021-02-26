afb0b99efa6deea396d79fda7e94116b7e556b0c
BoYanZh <bomingzh@sjtu.edu.cn>
2021-02-27 03:14:05 +0800

    enable check_untyped_defs & no_implicit_reexport in mypy

--- joj/horse/models/__init__.py
+++ joj/horse/models/__init__.py
@@ -1,7 +1,7 @@
-from joj.horse.models.domain import Domain
-from joj.horse.models.domain_role import DomainRole
-from joj.horse.models.domain_user import DomainUser
-from joj.horse.models.problem import Problem
-from joj.horse.models.problem_set import ProblemSet
-from joj.horse.models.record import Record
-from joj.horse.models.user import User
+from joj.horse.models.domain import Domain as Domain
+from joj.horse.models.domain_role import DomainRole as DomainRole
+from joj.horse.models.domain_user import DomainUser as DomainUser
+from joj.horse.models.problem import Problem as Problem
+from joj.horse.models.problem_set import ProblemSet as ProblemSet
+from joj.horse.models.record import Record as Record
+from joj.horse.models.user import User as User
--- joj/horse/models/base.py
+++ joj/horse/models/base.py
@@ -1,15 +1,16 @@
 from bson import ObjectId
+from umongo.frameworks.motor_asyncio import MotorAsyncIODocument
 
 from joj.horse.utils.errors import UnprocessableEntityError
 
 
 class DocumentMixin:
     @classmethod
-    async def find_by_id(cls, _id):
+    async def find_by_id(cls: MotorAsyncIODocument, _id):
         if not ObjectId.is_valid(_id):
             raise UnprocessableEntityError("Invalid ObjectId")
         return await cls.find_one({"_id": ObjectId(_id)})
 
     @classmethod
-    def aggregate(cls, pipeline, **kwargs):
+    def aggregate(cls: MotorAsyncIODocument, pipeline, **kwargs):
         return cls.collection.aggregate(pipeline, **kwargs)
