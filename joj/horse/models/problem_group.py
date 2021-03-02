82589decc8d2fc871b620338a25975e9ba22b550
tc-imba <liuyh615@sjtu.edu.cn>
2021-03-03 02:00:45 +0800

    fix problem group

--- joj/horse/models/problem_group.py
+++ joj/horse/models/problem_group.py
@@ -4,9 +4,6 @@ from pymongo import IndexModel
 from umongo import fields
 from umongo.frameworks.motor_asyncio import MotorAsyncIODocument
 
-if typing.TYPE_CHECKING:
-    from joj.horse.models.problem import Problem
-
 from joj.horse.models.base import DocumentMixin
 from joj.horse.utils.db import instance
 
@@ -15,8 +12,3 @@ from joj.horse.utils.db import instance
 class ProblemGroup(DocumentMixin, MotorAsyncIODocument):
     class Meta:
         collection_name = "problem.groups"
-        indexes = [IndexModel("problems")]  # TODO: appropriate indexes
-
-    problems = fields.ListField(
-        fields.ReferenceField("Problem", required=True), default=[]
-    )
