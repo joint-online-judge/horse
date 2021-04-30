2d3cc61b117905ecfb5ca143363840f652b2cc47
BoYanZh <bomingzh@sjtu.edu.cn>
2021-05-01 04:05:24 +0800

    test: better misc & domain test

--- joj/horse/tests/apis/test_domains.py
+++ joj/horse/tests/apis/test_domains.py
@@ -12,38 +12,81 @@ base_user_url = get_base_url(user)
 base_domain_url = get_base_url(domains)
 
 
-def test_create_user_domain(
+domain = schemas.DomainCreate(
+    url=random_lower_string(),
+    name=random_lower_string(),
+    bulletin=random_lower_string(),
+    gravatar=random_lower_string(),
+)
+data = jsonable_encoder(domain)
+domain_edit = schemas.DomainEdit(
+    name=random_lower_string(),
+    bulletin=random_lower_string(),
+    gravatar=random_lower_string(),
+)
+update_data = jsonable_encoder(domain_edit)
+NEW_DOMAIN = {}
+
+
+def test_create_domain(
     client: TestClient, test_user_token_headers: Dict[str, str], test_user: User
 ) -> None:
-    domain = schemas.DomainCreate(
-        url=random_lower_string(),
-        name=random_lower_string(),
-        bulletin=random_lower_string(),
-        gravatar=random_lower_string(),
-    )
-    data = jsonable_encoder(domain)
+    global NEW_DOMAIN
     r = client.post(f"{base_domain_url}", json=data, headers=test_user_token_headers)
     assert r.status_code == 200
     res = r.json()
-    new_domain = res
+    NEW_DOMAIN = res
     assert res["id"]
     assert res["url"] == domain.url
     assert res["name"] == domain.name
     assert res["bulletin"] == domain.bulletin
     assert res["gravatar"] == domain.gravatar
     assert res["owner"] == str(test_user.id)
+
+
+def test_list_user_domains(
+    client: TestClient, test_user_token_headers: Dict[str, str], test_user: User
+) -> None:
     r = client.get(f"{base_user_url}/domains", headers=test_user_token_headers)
     assert r.status_code == 200
     res = r.json()
-    assert res[0]["domain"] == new_domain
+    assert res[0]["domain"] == NEW_DOMAIN
     assert res[0]["user"] == str(test_user.id)
+
+
+def test_list_domains(
+    client: TestClient, test_user_token_headers: Dict[str, str], test_user: User
+) -> None:
     r = client.get(f"{base_domain_url}", headers=test_user_token_headers)
     assert r.status_code == 200
     res = r.json()
-    assert res[0] == new_domain
+    assert res[0] == NEW_DOMAIN
+
+
+def test_get_domain(
+    client: TestClient, test_user_token_headers: Dict[str, str], test_user: User
+) -> None:
     r = client.get(f"{base_domain_url}/{domain.url}", headers=test_user_token_headers)
     assert r.status_code == 200
     res = r.json()
-    assert new_domain["owner"] == res["owner"]["id"]
-    res["owner"] = new_domain["owner"]
-    assert res == new_domain
+    assert NEW_DOMAIN["owner"] == res["owner"]["id"]
+    res["owner"] = NEW_DOMAIN["owner"]
+    assert res == NEW_DOMAIN
+
+
+def test_update_domain(
+    client: TestClient, test_user_token_headers: Dict[str, str], test_user: User
+) -> None:
+    r = client.patch(
+        f"{base_domain_url}/{domain.url}",
+        json=update_data,
+        headers=test_user_token_headers,
+    )
+    assert r.status_code == 200
+    res = r.json()
+    assert res["id"]
+    assert res["url"] == domain.url
+    assert res["name"] == domain_edit.name
+    assert res["bulletin"] == domain_edit.bulletin
+    assert res["gravatar"] == domain_edit.gravatar
+    assert res["owner"] == str(test_user.id)
--- joj/horse/tests/apis/test_misc.py
+++ joj/horse/tests/apis/test_misc.py
@@ -1,23 +1,35 @@
+from typing import Dict
+
+import jwt
 from fastapi.testclient import TestClient
 
 from joj.horse.apis.misc import router_prefix
+from joj.horse.models.user import User
+from joj.horse.utils.version import get_git_version, get_version
 
 
 def test_version(client: TestClient) -> None:
     r = client.get(f"{router_prefix}/version")
     res = r.json()
     assert r.status_code == 200
-    assert res["version"]
-    assert res["git"]
+    assert res["version"] == get_version()
+    assert res["git"] == get_git_version()
 
 
-def test_jwt(client: TestClient) -> None:
-    r = client.get(
-        f"{router_prefix}/jwt",
-        cookies={
-            "jwt": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2MDNlODA0ODc3YWIwNzk2MDliYTkyMTIiLCJpYXQiOjE2MTkwMjQwNzEsIm5iZiI6MTYxOTAyNDA3MSwianRpIjoiNmQ0ZjQ0NWQtODJhNi00YThhLTg4MjktOTQwODIyYTIxNDhlIiwiZXhwIjoxNjIwMjMzNjcxLCJ0eXBlIjoiYWNjZXNzIiwiZnJlc2giOmZhbHNlLCJuYW1lIjoiYm9taW5nemgiLCJzY29wZSI6InNqdHUiLCJjaGFubmVsIjoiamFjY291bnQifQ.1j1NdhgUXRTKUReXWWgVDG96_Ryp50R3znVTpCGT5GY"
-        },
-    )
+def test_jwt(
+    client: TestClient, test_user_token_headers: Dict[str, str], test_user: User
+) -> None:
+    r = client.get(f"{router_prefix}/jwt", headers=test_user_token_headers)
     res = r.json()
     assert r.status_code == 200
-    assert res["jwt"]
+    res_jwt_dict = jwt.decode(res["jwt"], verify=False)
+    header_jwt_dict = jwt.decode(
+        test_user_token_headers["Authorization"][7:], verify=False
+    )
+    assert res_jwt_dict["sub"] == header_jwt_dict["sub"]
+    assert res_jwt_dict["type"] == header_jwt_dict["type"]
+    assert res_jwt_dict["fresh"] == header_jwt_dict["fresh"]
+    assert res_jwt_dict["name"] == header_jwt_dict["name"]
+    assert res_jwt_dict["scope"] == header_jwt_dict["scope"]
+    assert res_jwt_dict["channel"] == header_jwt_dict["channel"]
+    assert res_jwt_dict.get("csrf") == header_jwt_dict.get("csrf")
