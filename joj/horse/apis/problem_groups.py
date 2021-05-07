1e09e49717c336fbe29706b97975b5cc821cd5c4
BoYanZh <bomingzh@sjtu.edu.cn>
2021-05-08 03:00:04 +0800

    feat: error code in /problem_groups

--- joj/horse/apis/problem_groups.py
+++ joj/horse/apis/problem_groups.py
@@ -1,46 +1,45 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, Query
-from starlette.responses import Response
-from uvicorn.config import logger
+from fastapi import Depends
+from fastapi_utils.inferring_router import InferringRouter
 
 from joj.horse import models, schemas
+from joj.horse.schemas import StandardResponse
+from joj.horse.schemas.problem import ListProblems
+from joj.horse.schemas.problem_group import ListProblemGroups
 from joj.horse.utils.auth import Authentication
-from joj.horse.utils.db import instance
-from joj.horse.utils.errors import (
-    DeleteProblemBadRequestError,
-    InvalidAuthenticationError,
-    ProblemNotFoundError,
-)
-from joj.horse.utils.parser import (
-    parse_problem,
-    parse_problem_group,
-    parse_problem_set,
-    parse_uid,
-)
+from joj.horse.utils.parser import parse_problem_group
 
-router = APIRouter()
+router = InferringRouter()
 router_name = "problem_groups"
 router_tag = "problem group"
 router_prefix = "/api/v1"
 
 
-@router.get("", response_model=List[schemas.ProblemGroup])
+@router.get("")
 async def list_problem_groups(
     auth: Authentication = Depends(),
-) -> List[schemas.ProblemGroup]:
-    return [
-        schemas.ProblemGroup.from_orm(problem)
-        async for problem in models.ProblemGroup.find({})
-    ]
+) -> StandardResponse[ListProblemGroups]:
+    return StandardResponse(
+        ListProblemGroups(
+            rows=[
+                schemas.ProblemGroup.from_orm(problem)
+                async for problem in models.ProblemGroup.find({})
+            ]
+        )
+    )
 
 
-@router.get("/{problem_group}", response_model=List[schemas.Problem])
+@router.get("/{problem_group}")
 async def get_problems_in_problem_group(
     problem_group: models.ProblemGroup = Depends(parse_problem_group),
     auth: Authentication = Depends(),
-) -> List[schemas.Problem]:
-    return [
-        schemas.Problem.from_orm(problem)
-        async for problem in models.Problem.find({"group": problem_group.id})
-    ]
+) -> StandardResponse[ListProblems]:
+    return StandardResponse(
+        ListProblems(
+            rows=[
+                schemas.Problem.from_orm(problem)
+                async for problem in models.Problem.find({"group": problem_group.id})
+            ]
+        )
+    )
--- joj/horse/schemas/problem_group.py
+++ joj/horse/schemas/problem_group.py
@@ -1,9 +1,14 @@
 from typing import List
 
 from pydantic import HttpUrl
+from pydantic.main import BaseModel
 
 from joj.horse.schemas.base import BaseODMSchema
 
 
 class ProblemGroup(BaseODMSchema):
     moss_results: List[HttpUrl] = []
+
+
+class ListProblemGroups(BaseModel):
+    rows: List[ProblemGroup]
