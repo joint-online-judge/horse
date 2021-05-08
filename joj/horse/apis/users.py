351a4e749d37cc6ca3c032e299cc282b12bdf5f4
BoYanZh <bomingzh@sjtu.edu.cn>
2021-05-08 12:48:06 +0800

    feat: error code in /users

--- joj/horse/apis/users.py
+++ joj/horse/apis/users.py
@@ -3,6 +3,9 @@ from typing import List
 from fastapi import Depends
 
 from joj.horse import models, schemas
+from joj.horse.schemas import StandardResponse
+from joj.horse.schemas.domain_user import ListDomainMembers
+from joj.horse.schemas.problem import ListProblems
 from joj.horse.utils.auth import Authentication
 from joj.horse.utils.db import generate_join_pipeline
 from joj.horse.utils.parser import parse_uid
@@ -14,31 +17,39 @@ router_tag = "user"
 router_prefix = "/api/v1"
 
 
-@router.get("/{uid}", response_model=schemas.UserBase)
+@router.get("/{uid}")
 async def get_user(
     user: models.User = Depends(parse_uid), auth: Authentication = Depends()
-) -> schemas.UserBase:
-    return schemas.UserBase.from_orm(user)
+) -> StandardResponse[schemas.UserBase]:
+    return StandardResponse(schemas.UserBase.from_orm(user))
 
 
-@router.get("/{uid}/domains", response_model=List[schemas.DomainUser])
+@router.get("/{uid}/domains")
 async def get_user_domains(
     user: models.User = Depends(parse_uid),
-) -> List[schemas.DomainUser]:
+) -> StandardResponse[ListDomainMembers]:
     pipeline = generate_join_pipeline(field="domain", condition={"user": user.id})
-    return [
-        schemas.DomainUser.from_orm(
-            models.DomainUser.build_from_mongo(domain_user), unfetch_all=False
+    return StandardResponse(
+        ListDomainMembers(
+            rows=[
+                schemas.DomainUser.from_orm(
+                    models.DomainUser.build_from_mongo(domain_user), unfetch_all=False
+                )
+                async for domain_user in models.DomainUser.aggregate(pipeline)
+            ]
         )
-        async for domain_user in models.DomainUser.aggregate(pipeline)
-    ]
+    )
 
 
 @router.get("/{uid}/problems", response_model=List[schemas.Problem])
 async def get_user_problems(
     user: models.User = Depends(parse_uid),
-) -> List[schemas.Problem]:
-    return [
-        schemas.Problem.from_orm(problem)
-        async for problem in models.Problem.find({"owner": user.id})
-    ]
+) -> StandardResponse[ListProblems]:
+    return StandardResponse(
+        ListProblems(
+            rows=[
+                schemas.Problem.from_orm(problem)
+                async for problem in models.Problem.find({"owner": user.id})
+            ]
+        )
+    )
