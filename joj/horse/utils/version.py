20b3082160478dc9ed5cecd296c65e08d2ed43fc
BoYanZh <bomingzh@sjtu.edu.cn>
2021-05-15 01:38:42 +0800

    feat: commit time in description

--- joj/horse/utils/version.py
+++ joj/horse/utils/version.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import git
 import pbr.version
 from uvicorn.config import logger
@@ -12,6 +14,18 @@ def get_git_version() -> str:
         return git.Repo(__file__, search_parent_directories=True).git.describe(
             always=True, tags=True
         )
+    except (git.InvalidGitRepositoryError, git.GitCommandError) as e:
+        logger.error("Failed to get repository: %s", repr(e))
+        return "unknown"
+
+
+def get_git_head_commit_time() -> str:
+    try:
+        return datetime.fromtimestamp(
+            git.Repo(
+                __file__, search_parent_directories=True
+            ).head.commit.committed_date
+        ).strftime("%Y-%m-%d %H:%M:%S")
     except (git.InvalidGitRepositoryError, git.GitCommandError) as e:
         logger.error("Failed to get repository: %s", repr(e))
         return "unknown"
