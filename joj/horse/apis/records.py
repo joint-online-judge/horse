484ef9ce98640de4e1ced9514299ce3c5f4f71e1
BoYanZh <bomingzh@sjtu.edu.cn>
2021-05-15 02:30:39 +0800

    feat: http post from tiger

--- joj/horse/apis/records.py
+++ joj/horse/apis/records.py
@@ -7,8 +7,8 @@ from motor.motor_asyncio import AsyncIOMotorGridFSBucket
 from uvicorn.config import logger
 
 from joj.horse import models, schemas
-from joj.horse.schemas.base import StandardResponse
-from joj.horse.schemas.record import ListRecords
+from joj.horse.schemas.base import Empty, StandardResponse
+from joj.horse.schemas.record import ListRecords, RecordCaseResult, RecordResult
 from joj.horse.utils.auth import Authentication
 from joj.horse.utils.db import get_db
 from joj.horse.utils.errors import BizError, ErrorCode
@@ -60,38 +60,68 @@ async def get_record_code(record: models.Record = Depends(parse_record)) -> Any:
     )
 
 
-@router.websocket("/{record}/ws")
-async def websocket_record(
-    record: str, websocket: WebSocket  # TODO: parse_record
-) -> None:
+# @router.websocket("/{record}/ws")
+# async def websocket_record(
+#     record: str, websocket: WebSocket  # TODO: parse_record
+# ) -> None:
+#     record_model: models.Record = await models.Record.find_by_id(record)
+#     if record_model is None:
+#         raise BizError(ErrorCode.RecordNotFoundError)
+#     await websocket.accept()
+#     while True:
+#         if data["done"] == True:
+#             break
+#         data = await websocket.receive_json()
+#         logger.info(f"receive from record ws: {data}")
+#         result = data["result"]
+#         record_model.update(result)
+#         await record_model.commit()
+
+
+# @router.websocket("/{record}/cases/ws")
+# async def websocket_record_cases(
+#     record: str, websocket: WebSocket  # TODO: parse_record
+# ) -> None:
+#     record_model: models.Record = await models.Record.find_by_id(record)
+#     if record_model is None:
+#         raise BizError(ErrorCode.RecordNotFoundError)
+#     await websocket.accept()
+#     while True:
+#         if data["done"] == True:
+#             break
+#         data = await websocket.receive_json()
+#         logger.info(f"receive from record cases ws: {data}")
+#         index = data["index"]
+#         result = data["result"]
+#         record_model.cases[index].update(result)
+#         await record_model.commit()
+
+
+@router.post("/{record}/http")
+async def http_record(
+    record: str, record_result: RecordResult
+) -> StandardResponse[Empty]:  # TODO: parse_record
     record_model: models.Record = await models.Record.find_by_id(record)
     if record_model is None:
         raise BizError(ErrorCode.RecordNotFoundError)
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_json()
-        logger.info(f"receive from record ws: {data}")
-        result = data["result"]
-        record_model.update(result)
-        await record_model.commit()
-        if data["done"] == True:
-            break
+    data = record_result.dict()
+    logger.info(f"receive from record http: {data}")
+    record_model.update(data)
+    await record_model.commit()
+    return StandardResponse()
 
 
-@router.websocket("/{record}/cases/ws")
-async def websocket_record_cases(
-    record: str, websocket: WebSocket  # TODO: parse_record
-) -> None:
+@router.post("/{record}/cases/http")
+async def http_record_cases(
+    record: str, record_case_result: RecordCaseResult  # TODO: parse_record
+) -> StandardResponse[Empty]:
     record_model: models.Record = await models.Record.find_by_id(record)
     if record_model is None:
         raise BizError(ErrorCode.RecordNotFoundError)
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_json()
-        logger.info(f"receive from record cases ws: {data}")
-        index = data["index"]
-        result = data["result"]
-        record_model.cases[index].update(result)
-        await record_model.commit()
-        if data["done"] == True:
-            break
+    data = record_case_result.dict()
+    logger.info(f"receive from record cases http: {data}")
+    index = data["index"]
+    result = data["result"]
+    record_model.cases[index].update(result)
+    await record_model.commit()
+    return StandardResponse()
