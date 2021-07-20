b7e2f4d40e36780de50e5f8553a1931294c9842a
tc-imba <liuyh970615@gmail.com>
2021-07-21 01:10:25 +0800

    feat: retry connecting db

--- joj/horse/utils/db.py
+++ joj/horse/utils/db.py
@@ -1,3 +1,9 @@
+from functools import lru_cache
+from typing import Any, Dict
+
+from tenacity import retry
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_exponential
 from tortoise import Tortoise
 from tortoise.exceptions import OperationalError
 from uvicorn.config import logger
@@ -5,7 +11,8 @@ from uvicorn.config import logger
 from joj.horse.config import settings
 
 
-async def init_tortoise() -> None:
+@lru_cache()
+def get_tortoise_config() -> Dict[str, Any]:
     tortoise_config = {
         "connections": {
             "default": {
@@ -30,6 +37,11 @@ async def init_tortoise() -> None:
         "Tortoise-ORM engine: %s.",
         tortoise_config["connections"]["default"]["engine"],  # type: ignore
     )
+    return tortoise_config
+
+
+async def init_tortoise() -> None:
+    tortoise_config = get_tortoise_config()
     try:
         await Tortoise.init(config=tortoise_config, _create_db=True, use_tz=True)
         logger.info("Database %s created.", settings.db_name)
@@ -41,3 +53,22 @@ async def init_tortoise() -> None:
     if settings.debug:
         logger.info("Tortoise-ORM generating schema.")
         await Tortoise.generate_schemas()
+
+
+@retry(stop=stop_after_attempt(5), wait=wait_exponential(2))
+async def try_init_db() -> None:
+    attempt_number = try_init_db.retry.statistics["attempt_number"]
+    try:
+        await init_tortoise()
+    except Exception as e:
+        max_attempt_number = try_init_db.retry.stop.max_attempt_number
+        msg = "Tortoise-ORM: initialization failed ({}/{})".format(
+            attempt_number, max_attempt_number
+        )
+        if attempt_number < max_attempt_number:
+            msg += ", trying again after {} second.".format(2 ** attempt_number)
+        else:
+            msg += "."
+        logger.error(e)
+        logger.warning(msg)
+        raise e
