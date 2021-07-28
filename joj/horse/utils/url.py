db4e8b45d74073dd02ab85a65e7963d278f255a4
BoYanZh <bomingzh@sjtu.edu.cn>
2021-07-29 02:35:32 +0800

    test: increase coverage

--- joj/horse/utils/url.py
+++ joj/horse/utils/url.py
@@ -5,6 +5,6 @@ def generate_url(*args: str, protocol: str = "http") -> str:
     assert protocol in ["http", "ws"]
     path = "/".join(args)
     if path and path[0] != "/":
-        path = "/" + path
+        path = "/" + path  # pragma: no cover
     prefix = settings.http_url_prefix if protocol == "http" else settings.ws_url_prefix
     return prefix + path
