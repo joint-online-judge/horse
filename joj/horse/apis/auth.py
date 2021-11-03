fed85d0ec7bebb1ac9c2ccec31b013c847f9de99
tc-imba <liuyh970615@gmail.com>
2021-11-03 18:46:22 +0800

    misc: add debug output
    Signed-off-by: tc-imba <liuyh970615@gmail.com>

--- joj/horse/apis/auth.py
+++ joj/horse/apis/auth.py
@@ -204,6 +204,8 @@ def get_oauth_router(
             user.login_at = datetime.now(tz=timezone.utc)
             user.login_ip = request.client.host
             await user.save_model()
+            logger.debug(request.headers.items())
+            logger.info("user oauth login: %s", user)
 
             access_token, refresh_token = auth_jwt_encode_user(
                 auth_jwt, user=user, oauth_name=oauth_profile.oauth_name
@@ -238,6 +240,8 @@ def get_auth_router(
         user.login_at = datetime.now(tz=timezone.utc)
         user.login_ip = request.client.host
         await user.save_model()
+        logger.debug(request.headers.items())
+        logger.info("user login: %s", user)
         access_token, refresh_token = auth_jwt_encode_user(auth_jwt, user=user)
         return await get_login_response(
             request, response, auth_jwt, auth_parameters, access_token, refresh_token
