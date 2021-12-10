bb1e7eead3c6bb1757b1b0919e35ca9ce960e74d
tc-imba <liuyh970615@gmail.com>
2021-12-11 05:36:07 +0800

    chore: move openapi.py to main cli
    Signed-off-by: tc-imba <liuyh970615@gmail.com>

--- joj/horse/__main__.py
+++ joj/horse/__main__.py
@@ -1,13 +1,25 @@
-from typing import List, Optional
+from pathlib import Path
+from typing import Any, List, Optional
 
+import click
+import rapidjson
 import uvicorn
+from fastapi import FastAPI
 
 from joj.horse.config import get_settings
 from joj.horse.utils.cli import cli_command
 
 
+@click.group(invoke_without_command=True)
+@click.help_option("--help", "-h")
+@click.pass_context
+def cli_group(ctx: Any) -> Any:
+    if ctx.invoked_subcommand is None:
+        ctx.invoke(serve)
+
+
 @cli_command()
-def main() -> None:  # pragma: no cover
+def serve() -> None:  # pragma: no cover
     settings = get_settings()
     reload_dirs: Optional[List[str]] = None
     if settings.debug:
@@ -23,5 +35,27 @@ def main() -> None:  # pragma: no cover
     )
 
 
+@click.command("openapi")
+@click.option("-o", "--output", type=click.Path(), required=False, default=None)
+@click.option("-v", "--version", type=str, default="1")
+def openapi(output: Optional[str], version: str) -> None:
+    from joj.horse.app import app
+
+    sub_app_path = f"/api/v{version}"
+    for route in app.routes:
+        sub_app = route.app
+        if isinstance(sub_app, FastAPI) and route.path == sub_app_path:
+            openapi_json = rapidjson.dumps(sub_app.openapi(), indent=2)
+            if output is None:
+                print(openapi_json)
+            else:
+                with Path(output).open("w", encoding="utf-8") as f:
+                    f.write(openapi_json)
+            exit(0)
+    exit(-1)
+
+
 if __name__ == "__main__":
-    main()
+    cli_group.add_command(serve)
+    cli_group.add_command(openapi)
+    cli_group()
